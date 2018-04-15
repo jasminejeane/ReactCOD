7306d37573807b5d7fa619335bab8004f0f7c27d
jasminejeane <jasminejeane@gmail.com>
2018-04-15 12:27:04 -0700

    changing pages

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,30 @@
 import React, {Fragment} from 'react';
+import {getFunName} from "../helpers";
 
 class StorePicker extends React.Component {
+
+  myInput = React.createRef();
+
+  goToStore = event => {
+    event.preventDefault();
+    // 1. stop form onSubmit
+    // 2.get text from input
+    const storeName = this.myInput.value.value;
+    // 3.
+    this.props.history.push(`/store/${storeName}`);
+  }
+componentDidMount(){
+  console.log("Mounted!!!");
+}
   render(){
     return (
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Please Enter A Store</h2>
-          <input type="text" required placeholder="Store Name"/>
+          <button onClick={this.handleClick}>Click me!</button>
+          <input type="text"
+            ref={this.myInput}
+            required placeholder="Store Name"
+            defaultValue={getFunName()}/>
           <button type="submit"> Visit Store </button>
         </form>
 
